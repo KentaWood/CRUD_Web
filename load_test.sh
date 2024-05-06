289c71914d8b62f4ad15b60ded6e674abccfe604
KentaWood <you@example.com>
2024-05-06 09:16:53 -0700

    lets see

--- load_test.sh
+++ load_test.sh
@@ -6,5 +6,5 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-	echo "$files" |  time parallel python3 -u load_tweets.py --db  postgresql://hello_flask:hello_flask@localhost:5432/hello_flask_dev --inputs $file
+	echo "$files" |  time parallel python3 -u load_tweets.py --db  postgresql://hello_flask:hello_flask@db:5432/hello_flask_dev --inputs $files
     #echo "$files" |  python3 -u load_tweets.py --db postgresql://hello_flask:hello_flask@localhost:5432/hello_flask_dev --inputs "$file"
